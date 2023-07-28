0e7efe9eb508382745054e72e5ade3cc86513a21
Oleksandr Kompanets <74772286+KompanetsOleksandr@users.noreply.github.com>
2023-07-28 17:19:22 +0300

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,19 @@
 const { test, expect } = require('@playwright/test');
-test('my test', async ({ page }) => {
+
+test('has title', async ({ page }) => {
+
   await page.goto('https://playwright.dev/');
-// Expect a title "to contain" a substring.
+
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-// Expect an attribute "to be strictly equal" to the value.
-  await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
-// Expect an element "to be visible".
-  await expect(page.locator('text=Learn more').first()).toBeVisible();
-await page.click('text=Get Started');
-  // Expect some text to be visible on the page.
-  await expect(page.locator('text=Introduction').first()).toBeVisible();
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
 });
